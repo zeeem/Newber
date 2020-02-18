635aed54f16055184ce9c6c4b3935f185238b6e8
Jessica D'Cunha <35900289+jrdcunha@users.noreply.github.com>
2020-02-18 09:28:22 -0700

    added Android studio project setup with Firebase

---
+++ Newber/app/src/main/java/com/cmput301w20t23/newber/views/MainActivity.java
@@ -0,0 +1,16 @@
+package com.cmput301w20t23.newber.views;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.cmput301w20t23.newber.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
