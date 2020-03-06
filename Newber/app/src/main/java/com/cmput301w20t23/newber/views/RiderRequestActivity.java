720fd77151f9e1c313757434de2402ee3962b348
Ibrahim Aly <ialy@ualberta.ca>
2020-03-06 16:08:32 -0700

    Added from and to buttons and getting places correctly

--- Newber/app/src/main/java/com/cmput301w20t23/newber/views/RiderRequestActivity.java
+++ Newber/app/src/main/java/com/cmput301w20t23/newber/views/RiderRequestActivity.java
@@ -11,6 +11,7 @@ import android.os.Bundle;
 import android.view.View;
 
 import com.cmput301w20t23.newber.R;
+import com.google.android.gms.common.api.Status;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -18,7 +19,14 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.UiSettings;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.libraries.places.api.Places;
+import com.google.android.libraries.places.api.model.Place;
+import com.google.android.libraries.places.widget.AutocompleteSupportFragment;
+import com.google.android.libraries.places.widget.listener.PlaceSelectionListener;
 import com.google.android.material.snackbar.Snackbar;
+
+import java.util.Arrays;
+import java.util.Locale;
 
 public class RiderRequestActivity extends AppCompatActivity implements OnMapReadyCallback,
         ActivityCompat.OnRequestPermissionsResultCallback {
@@ -26,6 +34,8 @@ public class RiderRequestActivity extends AppCompatActivity implements OnMapRead
     private GoogleMap googleMap;
     private View mainLayout;
     private static final int PERMISSION_REQUEST_LOCATION = 0;
+    private AutocompleteSupportFragment fromAutocompleteSupportFragment;
+    private AutocompleteSupportFragment toAutocompleteSupportFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,6 +46,54 @@ public class RiderRequestActivity extends AppCompatActivity implements OnMapRead
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        setUpAutoCompleteFragments();
+    }
+
+    public void setUpAutoCompleteFragments() {
+        fromAutocompleteSupportFragment = (AutocompleteSupportFragment)
+                getSupportFragmentManager().findFragmentById(R.id.from_autocomplete_fragment);
+
+        if (!Places.isInitialized()) {
+            Places.initialize(getApplicationContext(), "AIzaSyAGmBxEjzU68YpgCNWsldO73dFcwnj0BT8", Locale.CANADA);
+        }
+
+        fromAutocompleteSupportFragment.setPlaceFields(Arrays.asList(Place.Field.ID, Place.Field.NAME, Place.Field.LAT_LNG));
+
+        fromAutocompleteSupportFragment.setOnPlaceSelectedListener(new PlaceSelectionListener() {
+            @Override
+            public void onPlaceSelected(@NonNull Place place) {
+                System.out.println("From Place: " + place.getName() + ", latlng: " + place.getLatLng());
+                RiderRequestActivity.this.googleMap.moveCamera(CameraUpdateFactory.newLatLng(place.getLatLng()));
+            }
+
+            @Override
+            public void onError(@NonNull Status status) {
+                System.out.println("An error occurred: " + status);
+            }
+        });
+
+        toAutocompleteSupportFragment = (AutocompleteSupportFragment)
+                getSupportFragmentManager().findFragmentById(R.id.to_autocomplete_fragment);
+
+        if (!Places.isInitialized()) {
+            Places.initialize(getApplicationContext(), "AIzaSyAGmBxEjzU68YpgCNWsldO73dFcwnj0BT8", Locale.CANADA);
+        }
+
+        toAutocompleteSupportFragment.setPlaceFields(Arrays.asList(Place.Field.ID, Place.Field.NAME, Place.Field.LAT_LNG));
+
+        toAutocompleteSupportFragment.setOnPlaceSelectedListener(new PlaceSelectionListener() {
+            @Override
+            public void onPlaceSelected(@NonNull Place place) {
+                System.out.println("To Place: " + place.getName() + ", latlng: " + place.getLatLng());
+                RiderRequestActivity.this.googleMap.moveCamera(CameraUpdateFactory.newLatLng(place.getLatLng()));
+            }
+
+            @Override
+            public void onError(@NonNull Status status) {
+                System.out.println("An error occurred: " + status);
+            }
+        });
     }
 
     @Override
@@ -82,10 +140,7 @@ public class RiderRequestActivity extends AppCompatActivity implements OnMapRead
         UiSettings uiSettings = this.googleMap.getUiSettings();
         uiSettings.setAllGesturesEnabled(true);
         uiSettings.setZoomControlsEnabled(true);
-//        uiSettings.setZoomGesturesEnabled(true);
-//        uiSettings.setCompassEnabled(true);
-//        uiSettings.setRotateGesturesEnabled(true);
-//        uiSettings.setTiltGesturesEnabled(true);
+        uiSettings.setCompassEnabled(true);
     }
 
     @Override
@@ -103,7 +158,6 @@ public class RiderRequestActivity extends AppCompatActivity implements OnMapRead
 
         // Add a marker in Sydney and move the camera
         LatLng edmonton = new LatLng(53.5558749,-113.772903);
-        this.googleMap.addMarker(new MarkerOptions().position(edmonton));
         this.googleMap.moveCamera(CameraUpdateFactory.newLatLng(edmonton));
     }
 }
