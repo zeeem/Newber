14381566062c6c2c5c9aef1218ef0eb2f88fac67
Amy Hou <amyhou98@gmail.com>
2020-03-07 15:45:22 -0700

    linked main activity to create rider request activity

--- Newber/app/src/main/java/com/cmput301w20t23/newber/views/RiderRequestNoneFragment.java
+++ Newber/app/src/main/java/com/cmput301w20t23/newber/views/RiderRequestNoneFragment.java
@@ -1,9 +1,11 @@
 package com.cmput301w20t23.newber.views;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.fragment.app.Fragment;
 import com.cmput301w20t23.newber.R;
@@ -13,7 +15,20 @@ public class RiderRequestNoneFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.rider_main_fragment, container, false);
+        // Inflater of layout for this fragment
+        View view = inflater.inflate(R.layout.rider_main_fragment, container, false);
+
+        Button createRequestButton = view.findViewById(R.id.rider_create_request_button);
+
+        createRequestButton.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                Intent intent = new Intent(getActivity(), RiderRequestActivity.class);
+                startActivity(intent);
+            }
+        });
+        return view;
     }
 }
