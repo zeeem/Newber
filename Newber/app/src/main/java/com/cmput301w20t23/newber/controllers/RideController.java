51a1607316cc2bf961fc1861818c1e61b0bfa2bf
Amy Hou <amyhou98@gmail.com>
2020-03-11 22:09:51 -0600

    debugging in progress, send help

--- Newber/app/src/main/java/com/cmput301w20t23/newber/controllers/RideController.java
+++ Newber/app/src/main/java/com/cmput301w20t23/newber/controllers/RideController.java
@@ -16,7 +16,6 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
-import com.google.maps.android.SphericalUtil;
 
 import java.util.ArrayList;
 
