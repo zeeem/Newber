bb3f6d8d1273fefc36f47982e9263bde083366da
Amy Hou <amyhou98@gmail.com>
2020-03-11 22:02:25 -0600

    debugging

--- Newber/app/src/main/java/com/cmput301w20t23/newber/views/MainActivity.java
+++ Newber/app/src/main/java/com/cmput301w20t23/newber/views/MainActivity.java
@@ -21,12 +21,17 @@ import com.cmput301w20t23.newber.models.RequestStatus;
 import com.cmput301w20t23.newber.models.RideRequest;
 import com.cmput301w20t23.newber.models.Rider;
 import com.cmput301w20t23.newber.models.User;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.libraries.places.api.model.Place;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 /**
  * The Android Activity that acts as the main user screen of the app.
@@ -103,6 +108,7 @@ public class MainActivity extends AppCompatActivity {
                         public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
                             if (dataSnapshot.exists()) {
                                 currRequest = dataSnapshot.getValue(RideRequest.class);
+//                                updateUsers();
                                 displayFragment();
                             }
                         }
@@ -123,6 +129,72 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+    }
+
+    public void updateUsers() {
+        switch (role) {
+            case "Rider":
+                // Get information of driver associated with current request
+                System.out.println("driver uid is: " + currRequest.getDriverUid());
+                if (currRequest.getDriverUid() != null && !currRequest.getDriverUid().isEmpty()) {
+                    System.out.println("I'm in");
+                    database.getReference("users").child(currRequest.getDriverUid()).addValueEventListener(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                            firstName = dataSnapshot.child("firstName").getValue(String.class);
+                            lastName = dataSnapshot.child("lastName").getValue(String.class);
+                            username = dataSnapshot.child("username").getValue(String.class);
+                            phone = dataSnapshot.child("phone").getValue(String.class);
+                            email = dataSnapshot.child("email").getValue(String.class);
+                            uId = currRequest.getDriverUid();
+                        }
+
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                        }
+                    });
+
+                    database.getReference("drivers").child(currRequest.getDriverUid()).addValueEventListener(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                            Rating rating = dataSnapshot.getValue(Rating.class);
+                            System.out.println("before driver make, username is: " + username);
+                            driver = new Driver(firstName, lastName, username, phone, email, uId, currRequest.getRequestId(), rating);
+                            System.out.println("driver added from rider");
+                        }
+
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                        }
+                    });
+                }
+                break;
+
+            case "Driver":
+                // Get rider associated with current request
+                System.out.println("in the rider");
+                database.getReference("users").child(currRequest.getRiderUid()).addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                        System.out.println("In datachange");
+                        firstName = dataSnapshot.child("firstName").getValue(String.class);
+                        lastName = dataSnapshot.child("lastName").getValue(String.class);
+                        username = dataSnapshot.child("username").getValue(String.class);
+                        phone = dataSnapshot.child("phone").getValue(String.class);
+                        email = dataSnapshot.child("email").getValue(String.class);
+                        uId = currRequest.getRiderUid();
+                        rider = new Rider(firstName, lastName, username, phone, email, uId, currRequest.getRequestId());
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                    }
+                });
+                break;
+        }
     }
 
     public void displayFragment() {
@@ -147,6 +219,8 @@ public class MainActivity extends AppCompatActivity {
                 case OFFERED:
                     statusBanner.setText("Offered");
                     statusBanner.setBackgroundColor(Color.rgb(255,165,0)); // orange
+                    updateUsers();
+                    System.out.println(driver);
                     riderFragment = new RequestOfferedFragment(currRequest, role, rider, driver);
                     break;
                 case ACCEPTED:
@@ -168,7 +242,7 @@ public class MainActivity extends AppCompatActivity {
 
         FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
         ft.replace(R.id.rider_request_details, riderFragment);
-        ft.commit();
+        ft.commitAllowingStateLoss();
     }
 
     @Override
