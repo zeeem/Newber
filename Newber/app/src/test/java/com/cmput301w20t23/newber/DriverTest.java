49374dd07d753b45543978743077f3838b60aa69
ARBNonay32 <60768976+ARBNonay32@users.noreply.github.com>
2020-03-13 14:51:27 -0600

    Update DriverTest.java

--- Newber/app/src/test/java/com/cmput301w20t23/newber/DriverTest.java
+++ Newber/app/src/test/java/com/cmput301w20t23/newber/DriverTest.java
@@ -5,6 +5,7 @@ import com.cmput301w20t23.newber.models.Location;
 import com.cmput301w20t23.newber.models.Rating;
 import com.cmput301w20t23.newber.models.RequestStatus;
 import com.cmput301w20t23.newber.models.RideRequest;
+import com.cmput301w20t23.newber.models.Rider;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -30,7 +31,9 @@ public class DriverTest {
         testEnd.setLatitude(74.0060);
         testStart.setName("New York");
 
-        testRequest = new RideRequest("123", testStart, testEnd, RequestStatus.PENDING, "456", "789", 10);
+        Rider testRider = new Rider();
+
+        testRequest = new RideRequest("123", testStart, testEnd, RequestStatus.PENDING, testRider, testDriver, 10);
         testDriver = new Driver("first",
                 "last",
                 "user",
@@ -39,39 +42,50 @@ public class DriverTest {
                 "69420",
                 testRequest.getRequestId(),
                 testRating);
+
+        testDriver.setCurrentRequest(testRequest);
     }
 
     @Test
     public void testSetAndGetRating(){
         Rating result = testDriver.getRating();
         Rating expected = new Rating(0, 0);
-        assertEquals(true, expected.equals(result));
+        assertEquals(true, expected.getUpvotes() == result.getUpvotes() && expected.getDownvotes() == result.getDownvotes());
 
         Rating newRating = new Rating(1, 6);
         testDriver.setRating(newRating);
         expected.setUpvotes(1);
         expected.setDownvotes(6);
         result = testDriver.getRating();
-        assertEquals(true, expected.equals(result));
+        assertEquals(true, expected.getUpvotes() == result.getUpvotes() && expected.getDownvotes() == result.getDownvotes());
     }
 
     @Test
     public void testSetAndGetCurrentRequest(){
         RideRequest result = testDriver.getCurrentRequest();
         RideRequest expected = testRequest;
-        assertEquals(true, expected.equals(result));
+        assertEquals(true, expected.getRequestId() == result.getRequestId()
+                && expected.getStartLocation() == result.getStartLocation()
+                && expected.getEndLocation() == result.getEndLocation()
+                && expected.getStatus() == result.getStatus()
+                && expected.getCost() == result.getCost()
+        );
+
 
         expected.setRequestId("321");
         expected.setStartLocation(testEnd);
         expected.setEndLocation(testStart);
         expected.setStatus(RequestStatus.COMPLETED);
-        expected.setRiderUid("654");
-        expected.setDriverUid("987");
         expected.setCost(16);
 
         testDriver.setCurrentRequest(expected);
         result = testDriver.getCurrentRequest();
-        assertEquals(true, expected.equals(result));
+        assertEquals(true, expected.getRequestId() == result.getRequestId()
+                && expected.getStartLocation() == result.getStartLocation()
+                && expected.getEndLocation() == result.getEndLocation()
+                && expected.getStatus() == result.getStatus()
+                && expected.getCost() == result.getCost());
+
 
     }
 }
